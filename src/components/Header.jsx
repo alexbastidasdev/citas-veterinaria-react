3baced8c6ee8141b9c0745c84dcce85149c433ed
Alex <alexbastidas.dev@gmail.com>
2022-10-02 18:15:20 -0500

    correccion header 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ function Header() {
   return (
 
     <h1 className="font-black text-4xl text-center md:w-2/3 mx-auto">
-      Seguiemiento Pacientes
+      Seguimiento Pacientes
       <span className="text-indigo-600"> Veterinaria</span>
     </h1>
 
